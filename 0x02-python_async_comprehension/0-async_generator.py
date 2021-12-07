31e92612bec8d7d941ff4af60606b486d3cbcdf1
EugeneJoe <jmuthui70@gmail.com>
2021-12-07 11:30:19 +0000

    Update type-annotation

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -4,12 +4,12 @@ Defines a coroutine that loops 10 times.
 In each loop it asynchronously waits 1 second,
 then yields a random number between 0 and 10.
 """
-from typing import Iterator
+from typing import Generator
 import random
 import asyncio
 
 
-async def async_generator() -> Iterator[float]:
+async def async_generator() -> Generator[float, None, None]:
     """
     async generator that waits 1 second and yields a random number between 0-10
     """
